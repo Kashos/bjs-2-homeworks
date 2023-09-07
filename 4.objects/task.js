725e82aa8cacb3f220d5047159b46c333b101d97
Egor Kachesov <kashos152@gmail.com>
2023-09-07 20:55:19 +0700

    Исправление ошибок №1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -32,5 +32,5 @@ Student.prototype.getAverage = function () {
 Student.prototype.exclude = function (reason) {
     delete this.subject;
     delete this.marks;
-    this.exclude = reason;
+    this.excluded = reason;
 }
